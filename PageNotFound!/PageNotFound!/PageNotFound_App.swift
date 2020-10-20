96067d05d9c3cc919502ab8517b1cbd4480c1bfe
Furkan Hanci <60580341+Furkanus@users.noreply.github.com>
2020-10-20 14:49:35 +0300

    Add files via upload

---
+++ PageNotFound!/PageNotFound!/PageNotFound_App.swift
@@ -0,0 +1,17 @@
+//
+//  PageNotFound_App.swift
+//  PageNotFound!
+//
+//  Created by Furkan Hanci on 10/20/20.
+//
+
+import SwiftUI
+
+@main
+struct PageNotFound_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
